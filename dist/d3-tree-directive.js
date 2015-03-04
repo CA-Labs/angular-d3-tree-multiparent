ed86f284c4bd26621654e70447a5f5bf91ca6854
Jacek Dominiak <jacekdominiak@outlook.com>
2015-03-04 15:22:44 +0100

    added dist

---
+++ dist/d3-tree-directive.js
@@ -0,0 +1,99 @@
+"use strict";
+
+/**
+ * Angular D3 tree chart multi parent directive
+ * @description: directive is displaying a tree chart without main parent, starting the chart from the dept 1 onwards
+ * it eliminates all the duplicated children and plots the connection back to the single child object
+ * @author: Jacek Dominiak
+ * @copyright: Jacek Dominiak
+ * @created: 04/03/15
+ */
+
+function AngularD3multiParentDirective() {
+
+    function link(scope, element, attr) {
+
+        // specify graph defaults with fallbacks
+        var graph = {
+            width: attr.width || 1000,
+            height: attr.height || 900
+        };
+
+        // extend d3 with moveToFront function
+        // this function redraws the elements in order to put them always in the front
+        d3.selection.prototype.moveToFront = function () {
+            return this.each(function () {
+                this.parentNode.appendChild(this);
+            });
+        };
+
+        // draw the svg container for the full graph
+        var svg = d3.select(element[0]).append("svg:svg").attr("width", graph.width).attr("height", graph.height).append("svg:g").attr("transform", "translate(-" + graph.width * 0.2 + ", 0)");
+
+        // specify the diagonal path link draw function
+        var diagonal = d3.svg.diagonal().projection(function (d) {
+            return [d.y, d.x];
+        });
+
+        // define the tree and leave some space for the text
+        var tree = d3.layout.tree().size([graph.height, graph.width / 2]);
+
+        // calculate nodes
+        var nodes = tree.nodes(scope.data);
+
+        //make nodes unique by id
+        nodes = _.uniq(nodes, "id");
+
+        // recalculate the x poition of each of then node after the removal
+        _.each(nodes, function (o, i) {
+            var itemsOfTheSameDepth = _.where(nodes, { depth: o.depth });
+            var indexOfTheCurrentItem = _.indexOf(itemsOfTheSameDepth, o);
+            var intervalPerDepth = graph.height / itemsOfTheSameDepth.length;
+            nodes[i].x = intervalPerDepth / 2 + intervalPerDepth * indexOfTheCurrentItem;
+        });
+
+        // calculate links
+        var links = tree.links(nodes);
+
+        // remap the links to the appropriate targets
+        _.each(links, function (o, i) {
+            links[i].target = _.find(nodes, { id: o.target.id });
+        });
+
+        // draw links
+        var link = svg.selectAll("path").data(links).enter().append("svg:path").attr("class", function (d) {
+            return !!d.source ? d.source.id : "root";
+        }).classed("link", true).attr("d", diagonal);
+
+        // draw nodes
+        var node = svg.selectAll("g.node").data(nodes).enter().append("svg:g").attr("transform", function (d) {
+            return "translate(" + d.y + ", " + d.x + ")";
+        }).on("mouseover", function (d) {
+            d3.selectAll("." + d.id).classed("hover", true).moveToFront();
+        }).on("mouseout", function (d) {
+            d3.selectAll("." + d.id).classed("hover", false);
+        });
+
+        // append some node visualization
+        node.append("svg:circle").attr("r", 3);
+
+        // add text to represent the meaning of the node
+        node.append("svg:text").attr("dx", function (d) {
+            return d.children ? -8 : 8;
+        }).attr("dy", 3).classed("text", true).attr("text-anchor", function (d) {
+            return d.children ? "end" : "start";
+        }).text(function (d) {
+            return d.name;
+        });
+    }
+
+    return {
+        restrict: "E",
+        scope: {
+            data: "="
+        },
+        link: link
+    };
+}
+
+angular.module("d3-multi-parent", []).directive("treeMultiParent", AngularD3multiParentDirective);
